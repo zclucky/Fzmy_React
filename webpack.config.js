78b83464f4d6ee47df3095cb642039eb204ff5a6
guotingchaopr <guotingchaopr@gmail.com>
2016-03-08 16:56:48 +0800

    init Project

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+module.exports = {
+
+ entry: path.resolve(__dirname,'app/main.js'),
+
+ output:{
+
+    path: path.resolve(__dirname,'build'),
+
+    filename:"bundle.js"
+
+ },
+ resolve: {
+        extensions: ['', '.js', '.jsx']
+ },
+ module: {
+    loaders: [
+        { test: /\.js|jsx$/,exclude: /node_modules/, loader: ['babel'],query: {presets: ['es2015', 'react']}}
+    ]
+ },
+ state:{
+ 	colors:true
+ },
+ devtool: 'source-map'
+}
