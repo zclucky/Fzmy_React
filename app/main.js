27dad8fb9d0ee4622957bdc27148274e0862d917
guotingchaopr <guotingchaopr@gmail.com>
2016-03-08 17:03:04 +0800

    add Entry File

---
+++ app/main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from  'react-dom';
+
+
+class Test extends React.Component{
+	render(){
+		return (
+			<div>
+				<h1>Hello Wolrd!</h1>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<Test /> , document.querySelector("#test"));
