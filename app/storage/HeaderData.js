3dd89f9307e623e48fd6cfc3343ce673418e81ca
zclucky <qaz_3344@126.com>
2016-03-21 17:35:54 +0800

    首页动态加载顶部导航栏及路由跳转

--- app/storage/HeaderData.js
+++ app/storage/HeaderData.js
@@ -1,3 +1,4 @@
+const titleStorage=localStorage.getItem('userDetail')||'未登录';
 const HeaderData = {
   title: '法治绵阳',
   link: '#title-link',
@@ -5,21 +6,21 @@ const HeaderData = {
     left: [
       {
         link: '#left-link',
-        title:'未登录',
-        icon: 'user'
+        title:titleStorage,
       }
     ],
     right: [
       {
         link: '#right-link',
-        icon: 'bell'
+        icon: 'bell',
       }
     ]
   },
   onSelect: function(nav, e) {
     e.preventDefault();
     console.log('你点击了', nav,e.target);
-    // do something
+    let router = this._reactInternalInstance._context.router;
+    router.replace(nav.link);
   }
 };
 export default HeaderData;
